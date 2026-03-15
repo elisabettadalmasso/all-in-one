a776c487ce7902f0c32520dd687678c5826a05c8
elisabettadalmasso <elisabettadalmasso96@gmail.com>
2026-03-15 15:02:57 +0100

    add hero slideshow with fade transition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,27 @@
 import { Link } from 'react-router-dom'
+import "./Navbar.css"
+import { useState } from 'react'
 
 function Navbar() {
+
+    const [isOpen,setIsOpen] = useState(false)
+
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/servizi">Servizi</Link>
-            <Link to="/eventi">Eventi</Link>
-            <Link to="/chi-siamo">Chi Siamo</Link>
-            <Link to="/contatti">Contatti</Link>
+            <Link to="/"><img src="/logoAllioneB.png" alt="All In One" className="navbar-logo" /></Link>
+
+             <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <div className={`navbar-links ${isOpen? "isOpen" : ""}`}>
+            <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+            <Link to="/servizi" onClick={() => setIsOpen(false)}>Servizi</Link>
+            <Link to="/eventi" onClick={() => setIsOpen(false)}>Eventi</Link>
+            <Link to="/chi-siamo" onClick={() => setIsOpen(false)}>Chi Siamo</Link>
+            <Link to="/contatti" onClick={() => setIsOpen(false)}>Contatti</Link>
+            </div>
         </nav>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react"
+import "./Home.css"
+
 function Home () {
+
+    const [foto,setFoto] = useState([])
+    const [fotoAttiva,setFotoAttiva] = useState(0)
+
+useEffect(() => {
+    async function scambiaHero() {
+        const trans = await fetch("hero.json")
+        const res = await trans.json()
+        setFoto(res)
+    }
+    scambiaHero()
+},[])
+
+useEffect(() => {
+    const timer = setInterval(() => {
+        setFotoAttiva(prev => (prev + 1) % foto.length)
+    }, 6000);
+    return() => clearInterval(timer)
+},[foto])
+
 return(
-    <h1>Home</h1>
+    <div className="hero">
+        <img 
+        key={fotoAttiva}
+        src={foto[fotoAttiva]?.immagine} 
+        alt={foto[fotoAttiva]?.titolo} 
+        style={{objectPosition: foto[fotoAttiva]?.position}}/>
+        <h1>{foto[fotoAttiva]?.titolo}</h1>
+        
+    </div>
+    
 )
 }
 export default Home
